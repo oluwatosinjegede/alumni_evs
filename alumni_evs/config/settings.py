400ef5b5787bd0e98540293d85fe2195c4c042c5
oluwatosinjegede <jegedeoluwatosin10@gmail.com>
2026-03-13 14:39:41 +0100

    Fix os import for templates

--- alumni_evs/config/settings.py
+++ alumni_evs/config/settings.py
@@ -1,3 +1,7 @@
+
+from pathlib import Path
+import os
+
 """
 Django settings for config project.
 
@@ -10,7 +14,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
